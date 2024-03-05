6c64db4a95611c793f58c9cb6450efda5fb68d9e
killian2301 <killianjimenez@gmail.com>
2024-03-05 09:15:29 +0000

    red: should return a list of heroes

---
+++ src/app/core/services/hero.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+
+import { HeroService } from './hero.service';
+
+describe('HeroService', () => {
+  let service: HeroService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(HeroService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return a list of heroes', () => {
+    const heroes = service.getHeroes();
+    expect(heroes.length).toBeGreaterThan(0);
+  });
+});
