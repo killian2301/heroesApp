6931137b253c3d22075d28a057d636d82ad9f963
killian2301 <killianjimenez@gmail.com>
2024-03-05 08:43:30 +0000

    adding route to heroes list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/heroes/pages/heroes-list/heroes-list.component').then(
+        (c) => c.HeroesListComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
