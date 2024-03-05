2ccc292166c6a7644ed632deb1bceda3d23a714d
killian2301 <killianjimenez@gmail.com>
2024-03-05 08:55:48 +0000

    green: should show an empty message when there is no heroes in the list

--- src/app/features/heroes/pages/heroes-list/heroes-list.component.spec.ts
+++ src/app/features/heroes/pages/heroes-list/heroes-list.component.spec.ts
@@ -8,10 +8,9 @@ describe('HeroesListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeroesListComponent]
-    })
-    .compileComponents();
-    
+      imports: [HeroesListComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HeroesListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +18,11 @@ describe('HeroesListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show an empty message when there is no heroes in the list', () => {
+    const emptyMessage = 'No heroes found.';
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain(emptyMessage);
   });
 });
--- src/app/features/heroes/pages/heroes-list/heroes-list.component.ts
+++ src/app/features/heroes/pages/heroes-list/heroes-list.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-heroes-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './heroes-list.component.html',
-  styleUrl: './heroes-list.component.scss'
+  styleUrl: './heroes-list.component.scss',
 })
 export class HeroesListComponent {
-
+  heroes = [];
 }
