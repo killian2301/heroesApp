d20f41b1fd28b41318a7bcbe667ab9cc9c52f22d
killian2301 <killianjimenez@gmail.com>
2024-03-09 17:45:42 +0000

    creating hero-filter component

---
+++ src/app/features/heroes/components/hero-filter/hero-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-filter.component.html',
+  styleUrl: './hero-filter.component.scss'
+})
+export class HeroFilterComponent {
+
+}
