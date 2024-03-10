286fd9c36294bd4f0e75f91de0f99dc3b2fa6146
killian2301 <killianjimenez@gmail.com>
2024-03-10 08:37:53 +0000

    adding filter-input

--- src/app/features/heroes/components/hero-filter/hero-filter.component.ts
+++ src/app/features/heroes/components/hero-filter/hero-filter.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 import { HeroFilterService } from '../../services/hero-filter.service';
 
 @Component({
   selector: 'app-hero-filter',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './hero-filter.component.html',
   styleUrl: './hero-filter.component.scss',
 })
-export class HeroFilterComponent {
+export class HeroFilterComponent implements OnInit, OnDestroy {
+  filterControl: FormControl = new FormControl('');
+  unsubscriber: Subject<boolean> = new Subject();
   constructor(private heroFilterService: HeroFilterService) {}
+
+  ngOnInit() {
+    this.filterControl.valueChanges
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe((query) => this.filter(query));
+  }
 
   filter(query: string) {
     this.heroFilterService.filter(query);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscriber.next(true);
+    this.unsubscriber.complete();
   }
 }
