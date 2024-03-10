fa8d9e466f653c7a028fb14ff7856ced5a3047b4
killian2301 <killianjimenez@gmail.com>
2024-03-10 11:18:53 +0000

    creating new hero component

---
+++ src/app/features/heroes/pages/new-hero/new-hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-hero.component.html',
+  styleUrl: './new-hero.component.scss'
+})
+export class NewHeroComponent {
+
+}
