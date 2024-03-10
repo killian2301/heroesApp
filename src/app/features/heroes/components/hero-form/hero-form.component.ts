919e002660738f6822fc3c2ba7a1a59eb0dbc169
killian2301 <killianjimenez@gmail.com>
2024-03-10 11:52:07 +0000

    green: should emit a hero when form is submited

--- src/app/features/heroes/components/hero-form/hero-form.component.ts
+++ src/app/features/heroes/components/hero-form/hero-form.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Hero } from '../../../../core/models/hero.model';
 
 @Component({
   selector: 'app-hero-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './hero-form.component.html',
-  styleUrl: './hero-form.component.scss'
+  styleUrl: './hero-form.component.scss',
 })
 export class HeroFormComponent {
+  heroForm!: FormGroup;
+  heroSubmitted: EventEmitter<Hero> = new EventEmitter();
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit() {
+    this.heroForm = this.fb.group({
+      name: [''],
+    });
+  }
+
+  onSubmit() {
+    this.heroSubmitted.emit(this.heroForm.value);
+  }
 }
--- src/app/features/heroes/pages/new-hero/new-hero.component.ts
+++ src/app/features/heroes/pages/new-hero/new-hero.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { HeroFormComponent } from '../../components/hero-form/hero-form.component';
 
 @Component({
   selector: 'app-new-hero',
   standalone: true,
-  imports: [],
+  imports: [HeroFormComponent],
   templateUrl: './new-hero.component.html',
-  styleUrl: './new-hero.component.scss'
+  styleUrl: './new-hero.component.scss',
 })
 export class NewHeroComponent {
-
+  onSubmit(hero: Event) {
+    console.log(hero);
+  }
 }
