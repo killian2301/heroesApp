f707ada13d4218da7166a34ae682672e77909996
killian2301 <killianjimenez@gmail.com>
2024-03-10 08:58:28 +0000

    refactor: using error handler service

--- src/app/features/heroes/services/hero-filter.service.ts
+++ src/app/features/heroes/services/hero-filter.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import {
   BehaviorSubject,
+  Observable,
   catchError,
   debounceTime,
   map,
-  of,
   switchMap,
 } from 'rxjs';
 import { Hero } from '../../../core/models/hero.model';
+import { ErrorHandlerService } from '../../../core/services/error-handler.service';
 import { HeroService } from '../../../core/services/hero.service';
 
 @Injectable({
@@ -16,40 +17,37 @@ import { HeroService } from '../../../core/services/hero.service';
 export class HeroFilterService {
   query: BehaviorSubject<string> = new BehaviorSubject<string>('');
   filteredHeroes: BehaviorSubject<Hero[]> = new BehaviorSubject<Hero[]>([]);
-  constructor(private heroService: HeroService) {
+  constructor(
+    private heroService: HeroService,
+    private errorHandler: ErrorHandlerService
+  ) {
     this.setUpSubscription();
   }
 
-  private setUpSubscription() {
+  private setUpSubscription(): void {
     this.query
       .pipe(
         debounceTime(500),
         switchMap((filterQuery) => this.fetchFilteredHeroes(filterQuery)),
-        catchError((error) => {
-          console.error(error);
-          return of([]);
-        })
+        catchError(this.errorHandler.handle)
       )
       .subscribe((heroes) => this.filteredHeroes.next(heroes));
   }
 
-  filter(query: string) {
+  filter(query: string): void {
     this.query.next(query);
   }
 
-  fetchFilteredHeroes(query: string) {
+  fetchFilteredHeroes(query: string): Observable<Hero[]> {
     return this.heroService.getHeroes().pipe(
       map((heroes) =>
         heroes.filter((hero) => hero.name.includes(query.toLowerCase()))
       ),
-      catchError((error) => {
-        console.error(error);
-        return of([] as Hero[]);
-      })
+      catchError(this.errorHandler.handle)
     );
   }
 
-  getFilteredHeroes() {
+  getFilteredHeroes(): Observable<Hero[]> {
     return this.filteredHeroes.asObservable();
   }
 }
