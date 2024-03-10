171b9771ed2f51c51bba53ed554731caf8c3f96d
killian2301 <killianjimenez@gmail.com>
2024-03-10 08:42:20 +0000

    using hero filter in heroes list

--- src/app/features/heroes/pages/heroes-list/heroes-list.component.spec.ts
+++ src/app/features/heroes/pages/heroes-list/heroes-list.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HeroesListComponent } from './heroes-list.component';
 
 describe('HeroesListComponent', () => {
@@ -8,7 +9,7 @@ describe('HeroesListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeroesListComponent],
+      imports: [HeroesListComponent, HttpClientTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeroesListComponent);
--- src/app/features/heroes/pages/heroes-list/heroes-list.component.ts
+++ src/app/features/heroes/pages/heroes-list/heroes-list.component.ts
@@ -1,15 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { Subject, takeUntil } from 'rxjs';
 import { Hero } from '../../../../core/models/hero.model';
+import { HeroFilterComponent } from '../../components/hero-filter/hero-filter.component';
 import { HeroTileComponent } from '../../components/hero-tile/hero-tile.component';
+import { HeroFilterService } from '../../services/hero-filter.service';
 @Component({
   selector: 'app-heroes-list',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, HeroTileComponent],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    HeroTileComponent,
+    HeroFilterComponent,
+    HttpClientModule,
+  ],
+  providers: [HeroFilterService],
   templateUrl: './heroes-list.component.html',
   styleUrl: './heroes-list.component.scss',
 })
-export class HeroesListComponent {
+export class HeroesListComponent implements OnInit, OnDestroy {
   heroes: Hero[] = [];
+  unsubscriber = new Subject();
+
+  constructor(private heroFilterService: HeroFilterService) {}
+
+  ngOnInit() {
+    this.heroFilterService
+      .getFilteredHeroes()
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscriber.next(true);
+    this.unsubscriber.complete();
+  }
 }
