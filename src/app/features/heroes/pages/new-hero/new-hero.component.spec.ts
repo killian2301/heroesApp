f664f833b330042d344a1150f2d9fb56cd64f31b
killian2301 <killianjimenez@gmail.com>
2024-03-10 15:31:24 +0000

    refactor: new hero

--- src/app/features/heroes/pages/new-hero/new-hero.component.spec.ts
+++ src/app/features/heroes/pages/new-hero/new-hero.component.spec.ts
@@ -1,19 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { HeroService } from '../../services/hero.service';
 import { NewHeroComponent } from './new-hero.component';
 
 describe('NewHeroComponent', () => {
   let component: NewHeroComponent;
   let fixture: ComponentFixture<NewHeroComponent>;
-
+  let router: Router;
+  let heroService: HeroService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NewHeroComponent, HttpClientTestingModule],
+      imports: [NewHeroComponent, HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        { provide: Router, useValue: { navigate: jest.fn() } },
+        { provide: HeroService, useValue: { saveHero: jest.fn() } },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NewHeroComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    heroService = TestBed.inject(HeroService);
     fixture.detectChanges();
   });
 
@@ -29,5 +40,14 @@ describe('NewHeroComponent', () => {
   it('should have a form', () => {
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('form')).toBeTruthy();
+  });
+
+  it('should return to heroes list once submitted successfully', () => {
+    jest
+      .spyOn(heroService, 'saveHero')
+      .mockReturnValue(of({ id: 1, name: 'Spiderman' }));
+    const spy = jest.spyOn(router, 'navigate');
+    component.onSubmit({ name: 'Spiderman' });
+    expect(spy).toHaveBeenCalledWith(['/heroes']);
   });
 });
--- src/app/features/heroes/pages/new-hero/new-hero.component.ts
+++ src/app/features/heroes/pages/new-hero/new-hero.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 import { Hero } from '../../../../core/models/hero.model';
 import { HeroFormComponent } from '../../components/hero-form/hero-form.component';
 import { HeroService } from '../../services/hero.service';
@@ -10,12 +12,22 @@ import { HeroService } from '../../services/hero.service';
   templateUrl: './new-hero.component.html',
   styleUrl: './new-hero.component.scss',
 })
-export class NewHeroComponent {
-  constructor(private heroService: HeroService) {}
+export class NewHeroComponent implements OnDestroy {
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  constructor(private heroService: HeroService, private router: Router) {}
 
   onSubmit(hero: Omit<Hero, 'id'>) {
-    this.heroService.saveHero(hero).subscribe((hero) => {
-      console.log(hero);
-    });
+    this.heroService
+      .saveHero(hero)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        complete: () => this.router.navigate(['/heroes']),
+        error: (error) => console.error('Failed to save hero', error),
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
