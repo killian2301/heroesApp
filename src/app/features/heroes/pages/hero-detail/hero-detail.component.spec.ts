639b9bae7d42f8feceb2376f853ea97d85810588
killian2301 <killianjimenez@gmail.com>
2024-03-10 09:37:44 +0000

    red: should show hero name in capitalize letters

--- src/app/features/heroes/pages/hero-detail/hero-detail.component.spec.ts
+++ src/app/features/heroes/pages/hero-detail/hero-detail.component.spec.ts
@@ -8,10 +8,9 @@ describe('HeroDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeroDetailComponent]
-    })
-    .compileComponents();
-    
+      imports: [HeroDetailComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HeroDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +18,12 @@ describe('HeroDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show hero name in capitalize letters', () => {
+    component.hero = { name: 'spiderman' };
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('SPIDERMAN');
   });
 });
