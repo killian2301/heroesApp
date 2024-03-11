5fb36de32b31f908d2dd4ee51312cdeece944179
killian2301 <killianjimenez@gmail.com>
2024-03-11 21:42:45 +0000

    adding notification service

---
+++ src/app/shared/services/notification.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  constructor(private snackBar: MatSnackBar) {}
+
+  openSnackBar(message: string, config: MatSnackBarConfig) {
+    return this.snackBar.open(message, '', config);
+  }
+
+  error(message: string) {
+    const config: MatSnackBarConfig = {
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
+      duration: 3000,
+    };
+    return this.openSnackBar(message, config);
+  }
+  success(message: string) {
+    const config: MatSnackBarConfig = {
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
+      duration: 3000,
+    };
+    return this.openSnackBar(message, config);
+  }
+}
