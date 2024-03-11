1a4f6074225153015e0b30e482fb3e381536b409
killian2301 <killianjimenez@gmail.com>
2024-03-11 07:52:24 +0000

    refactor: new hero

--- src/app/features/heroes/pages/new-hero/new-hero.component.ts
+++ src/app/features/heroes/pages/new-hero/new-hero.component.ts
@@ -13,37 +13,46 @@ import { HeroService } from '../../services/hero.service';
   styleUrl: './new-hero.component.scss',
 })
 export class NewHeroComponent implements OnDestroy {
+  private destroy$: Subject<boolean> = new Subject<boolean>();
   hero!: Hero;
-  destroy$: Subject<boolean> = new Subject<boolean>();
   mode: 'new' | 'edit' = 'new';
   constructor(
     private heroService: HeroService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
   ) {
-    this.activatedRoute.params
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((params) => {
+    this.initializeComponent();
+  }
+  onSubmit(hero: Omit<Hero, 'id'> | Hero): void {
+    this.mode === 'new'
+      ? this.saveNewHero(hero as Omit<Hero, 'id'>)
+      : this.saveEditedHero(hero as Hero);
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+  private initializeComponent() {
+    this.activatedRoute.params.pipe(takeUntil(this.destroy$)).subscribe({
+      next: (params) => {
         const heroId = params['id'];
         if (!heroId) return;
-
         this.mode = 'edit';
-        this.heroService
-          .getHero(heroId)
-          .pipe(takeUntil(this.destroy$))
-          .subscribe((hero) => (this.hero = hero));
+        this.loadHero(heroId);
+      },
+      error: (error) => console.error('Error loading hero', error),
+    });
+  }
+  private loadHero(heroId: number): void {
+    this.heroService
+      .getHero(heroId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (hero) => (this.hero = hero),
+        error: (error) => console.error('Failed to load hero', error),
       });
   }
-
-  onSubmit(hero: Omit<Hero, 'id'> | Hero) {
-    if (this.mode === 'new') {
-      this.saveNewHero(hero as Omit<Hero, 'id'>);
-    } else {
-      this.saveEditedHero(hero as Hero);
-    }
-  }
-
-  saveNewHero(hero: Omit<Hero, 'id'>) {
+  private saveNewHero(hero: Omit<Hero, 'id'>) {
     this.heroService
       .saveHero(hero)
       .pipe(takeUntil(this.destroy$))
@@ -52,8 +61,7 @@ export class NewHeroComponent implements OnDestroy {
         error: (error) => console.error('Failed to save hero', error),
       });
   }
-
-  saveEditedHero(hero: Hero) {
+  private saveEditedHero(hero: Hero) {
     this.heroService
       .updateHero(hero)
       .pipe(takeUntil(this.destroy$))
@@ -61,10 +69,5 @@ export class NewHeroComponent implements OnDestroy {
         complete: () => this.router.navigate(['/heroes']),
         error: (error) => console.error('Failed to save hero', error),
       });
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next(true);
-    this.destroy$.complete();
   }
 }
