254250031521d4ebbaead5e5f9047b6727c5b67c
killian2301 <killianjimenez@gmail.com>
2024-03-11 21:44:01 +0000

    adding properties to hero.model

--- src/app/core/models/hero.model.ts
+++ src/app/core/models/hero.model.ts
@@ -1,10 +1,26 @@
 export class Hero {
   id: number;
   name: string;
-
-  private constructor(name: string) {
-    this.id = Math.floor(Math.random() * 100);
+  biography?: {
+    publisher: string;
+  };
+  work: {
+    occupation: string;
+  };
+  images?: {
+    xs: string;
+    sm: string;
+    md: string;
+    lg: string;
+  };
+  appearance?: {
+    gender: string;
+    race: string;
+  };
+  private constructor(name: string, occupation: string = '-') {
+    this.id = Math.floor(Math.random() * 1000);
     this.name = name;
+    this.work = { occupation };
   }
 
   static create(heroForm: Omit<Hero, 'id'>): Hero {
