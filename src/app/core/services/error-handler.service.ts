58937fcf4314aaa9d37efae1539c90936a5753fa
killian2301 <killianjimenez@gmail.com>
2024-03-11 21:45:07 +0000

    adding notificationservice to error handler

--- src/app/core/services/error-handler.service.ts
+++ src/app/core/services/error-handler.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
+import { NotificationService } from '../../shared/services/notification.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ErrorHandlerService {
-  constructor() {}
+  constructor(private notificationService: NotificationService) {}
 
   handle(error: any, message: string, defaultValue?: any) {
     console.error(error);
     if (defaultValue) {
       return of(defaultValue);
     }
+    this.notificationService.error(message);
     throw new Error(message);
   }
 }
