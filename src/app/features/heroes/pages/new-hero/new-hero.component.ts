3c5ddeba6ee2778142cc8b2beefa6a1fed25d051
killian2301 <killianjimenez@gmail.com>
2024-03-12 09:47:53 +0000

    customizing new hero component

--- src/app/features/heroes/pages/new-hero/new-hero.component.ts
+++ src/app/features/heroes/pages/new-hero/new-hero.component.ts
@@ -1,15 +1,25 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component, NgZone, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { Hero } from '../../../../core/models/hero.model';
+import { HeaderComponent } from '../../../../layout/header/header.component';
 import { NotificationService } from '../../../../shared/services/notification.service';
+import { DeleteHeroComponent } from '../../components/delete-hero/delete-hero.component';
 import { HeroFormComponent } from '../../components/hero-form/hero-form.component';
 import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'app-new-hero',
   standalone: true,
-  imports: [HeroFormComponent],
+  imports: [
+    HeroFormComponent,
+    HeaderComponent,
+    RouterModule,
+    UpperCasePipe,
+    HeaderComponent,
+    DeleteHeroComponent,
+  ],
   templateUrl: './new-hero.component.html',
   styleUrl: './new-hero.component.scss',
 })
@@ -34,6 +44,11 @@ export class NewHeroComponent implements OnDestroy {
   ngOnDestroy(): void {
     this.destroy$.next(true);
     this.destroy$.complete();
+  }
+  onHeroDeleted(isDeleted: boolean) {
+    if (!isDeleted) return;
+    this.notificationService.success('Hero deleted');
+    this.goBackToHeroesList();
   }
   private initializeComponent() {
     const heroId = this.activatedRoute.snapshot.params['id'];
