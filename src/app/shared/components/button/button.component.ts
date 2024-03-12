ff99c19ae214207184c1d8bcf1af141589cd5d29
killian2301 <killianjimenez@gmail.com>
2024-03-12 08:53:33 +0000

    adding icons to buttons

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
   @Input() label: string = '';
   @Input() color?: 'warn' | 'accent' | 'primary';
+  @Input() matIconName?: string;
   @Input() disabled: boolean = false;
 }
