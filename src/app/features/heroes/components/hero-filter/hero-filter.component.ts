513898d6ce859c89fe790a91d137389aa9185ad4
killian2301 <killianjimenez@gmail.com>
2024-03-12 09:56:01 +0000

    removing unused Import

--- src/app/features/heroes/components/hero-filter/hero-filter.component.ts
+++ src/app/features/heroes/components/hero-filter/hero-filter.component.ts
@@ -1,18 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Subject, takeUntil } from 'rxjs';
 import { HeroFilterService } from '../../services/hero-filter.service';
 @Component({
   selector: 'app-hero-filter',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-  ],
+  imports: [ReactiveFormsModule, MatInputModule, FormsModule],
   templateUrl: './hero-filter.component.html',
   styleUrl: './hero-filter.component.scss',
 })
